1266a6e3d5ae82b14ed0f28813c3b2fbb4b70ce2
gabrieldev525 <gvictor525.gv@gmail.com>
2019-03-04 16:06:53 -0300

    the layout is now responsive to all devices. the about session is done

--- js/script.js
+++ js/script.js
@@ -6,8 +6,49 @@ $(window).on("scroll", function() {
     if(y >= $(window).height() / 3) {
         $("#menu").addClass("menu-style");
         $("#menu a").css("color", "#000");
+        $("#menu .side-item i").css("color", "#000");
     } else {
         $("#menu").removeClass("menu-style");
         $("#menu a").css("color", "#fff");
+        $("#menu .side-item i").css("color", "#fff");
     }
+});
+
+// close the menu with the window is bigger than 480px
+$(window).on("resize", function() {
+    var width = $(window).width();
+
+    if(width > 480) {
+        $("#mobile-menu").hide();
+    }
+});
+
+// open the mobile-menu
+$("#menu .side-item").click(function() {
+    $("#mobile-menu").show();
+}); 
+
+// close the mobile-menu
+$("#mobile-menu #close").click(function() {
+    $("#mobile-menu").hide();
+}); 
+
+$("#mobile-menu li").click(function() {
+    $("#mobile-menu").hide();
+});
+// mobile menu item link
+$("#mobile-menu #inicio-item").click(function() {
+    window.open("#","_self");
+});
+$("#mobile-menu #about-item").click(function() {
+    window.open("#about","_self");
+});
+$("#mobile-menu #projects-item").click(function() {
+    window.open("#projects","_self");
+});
+$("#mobile-menu #habilidades-item").click(function() {
+    window.open("#habilidades","_self");
+});
+$("#mobile-menu #contact-item").click(function() {
+    window.open("#contato","_self");
 });
